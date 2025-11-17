025fc528fcde4d7d5ded72b231487013d49305bb
GoGu <hugo.roche48@gmail.com>
2025-11-17 21:09:47 +0100

    [ADD] - cmake var for tests build

--- dependencies/dependencies.cmake
+++ dependencies/dependencies.cmake
@@ -4,12 +4,14 @@ include(FetchContent)
 FetchContent_Declare(
     curl
     GIT_REPOSITORY https://github.com/curl/curl.git
-    GIT_TAG        curl-8_11_0
+    GIT_TAG        curl-8_11_1
 )
 FetchContent_MakeAvailable(curl)
 
-FetchContent_Declare(
-    googletest
-    URL https://github.com/google/googletest/archive/refs/tags/v1.15.0.zip
-)
-FetchContent_MakeAvailable(googletest)
+if (DEFINED GOGULIB_BUILD_TESTS AND GOGULIB_BUILD_TESTS)
+    FetchContent_Declare(
+        googletest
+        URL https://github.com/google/googletest/archive/refs/tags/v1.15.0.zip
+    )
+    FetchContent_MakeAvailable(googletest)
+endif()
